19d3265fe877284e8ab048f92a26b2342afe0371
RS <rutujasakhare098@gmail.com>
2026-02-21 13:58:04 +0530

    Time: 2 ms (77.88%) | Memory: 8.4 MB (7.90%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.cpp
@@ -4,19 +4,13 @@ public:
         bitset<32> b(n);
         return b.count(); 
     }
-    bool isPrime(int n){
-        if(n==1) return false;
-        if(n==2) return true;
-        for(int i=2;i<n;i++){
-        if(n%i==0) return false;
-        }
-        return true;
-    }
+   
     int countPrimeSetBits(int left, int right) {
         int count=0;
+        unordered_set<int>s={2,3,5,7,11,13,17,19};
         for(int i=left;i<=right;i++){
             int a=FindSetBit(i);
-            if(isPrime(a)) count++;
+            if(s.find(a)!=s.end()) count++;
 
         }
         return count;
