273f08ddfb883e64e964df002d155ec718742c92
RS <rutujasakhare098@gmail.com>
2026-03-06 19:10:07 +0530

    Time: 3 ms (70.20%) | Memory: 15.6 MB (51.23%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+
+public:
+    void generate(string s, int open, int close, int n,vector<string>&ans)
+{
+  if (close == n)
+  {
+    ans.push_back(s);
+  }
+
+  if (open < n)
+    generate(s + '(', open + 1, close, n,ans);
+  if (open > close)
+    generate(s + ')', open, close + 1, n,ans);
+}
+    vector<string> generateParenthesis(int n) {
+     vector<string>ans;
+     generate("" , 0,0,n,ans);
+     return ans;
+        
+    }
+};
