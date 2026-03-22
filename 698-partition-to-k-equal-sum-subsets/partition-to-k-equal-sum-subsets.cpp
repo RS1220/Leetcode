24a3acdb4014b221492cf53ccc4816238daf238c
RS <rutujasakhare098@gmail.com>
2026-03-22 23:38:30 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (55.20%) - LeetSync

---
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool backtrack(int idx, vector<int>& nums, vector<int>& bucket, int target) {
+        if (idx == nums.size()) return true;
+
+        for (int i = 0; i < bucket.size(); i++) {
+            
+            // skip if adding exceeds target
+            if (bucket[i] + nums[idx] > target) continue;
+
+            // skip duplicate buckets
+            if (i > 0 && bucket[i] == bucket[i - 1]) continue;
+
+            bucket[i] += nums[idx];
+
+            if (backtrack(idx + 1, nums, bucket, target))
+                return true;
+
+            bucket[i] -= nums[idx];
+
+            // 🔥 important pruning
+            if (bucket[i] == 0) break;
+        }
+
+        return false;
+    }
+
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        int sum = 0;
+        for (int x : nums) sum += x;
+
+        if (sum % k != 0) return false;
+
+        int target = sum / k;
+
+        sort(nums.rbegin(), nums.rend()); // 🔥 big optimization
+
+        // if largest element > target → impossible
+        if (nums[0] > target) return false;
+
+        vector<int> bucket(k, 0);
+
+        return backtrack(0, nums, bucket, target);
+    }
+};
