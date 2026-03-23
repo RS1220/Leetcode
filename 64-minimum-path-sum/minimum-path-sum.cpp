bc3c771215cdf929eb621fb3447e5a0db9fb8441
RS <rutujasakhare098@gmail.com>
2026-03-23 23:54:29 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (15.50%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,16 +1,27 @@
 class Solution {
 public:
-    int minPathSum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        for(int j=1;j<m;j++) grid[0][j]+=grid[0][j-1];
-        for(int i=1;i<n;i++) grid[i][0]+=grid[i-1][0];
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                if(i==0 || j==0) grid[i][j]+=1;
-                else grid[i][j]+=min(grid[i-1][j],grid[i][j-1]);
-            }
+    int n, m;
+    vector<vector<int>>dp;
+    int helper(vector<vector<int>>& grid,int i, int j)
+    {    if(i>n || j>m) return 0;
+        if(i==n-1 && j==m-1) return grid[i][j];
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==n-1){
+            //call only right
+            return dp[i][j]=grid[i][j]+helper(grid,i,j+1);
         }
-        return grid[n-1][m-1];
+        else if(j==m-1){
+            //go only down
+            return dp[i][j]=grid[i][j]+helper(grid,i+1,j);
+        }
+       
+        else return dp[i][j]=grid[i][j]+min(helper(grid,i,j+1),helper(grid,i+1,j));
+    }
+    int minPathSum(vector<vector<int>>& grid) 
+    {
+          n=grid.size();
+          m=grid[0].size();
+          dp.resize(n,(vector<int>(m,-1)));
+         return helper(grid,0,0);
     }
 };
