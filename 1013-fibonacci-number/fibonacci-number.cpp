8a7c19d75efcbda2096394db18342dda899124dc
RS <rutujasakhare098@gmail.com>
2026-03-23 18:49:43 +0530

    Time: 13 ms (32.31%) | Memory: 7.9 MB (36.93%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,16 +1,8 @@
 class Solution {
 public:
-    //Tabulation
-    int fib(int n) {
-        if(n<=1) return n;
-        vector<int>dp(3);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-          dp[2]=dp[0]+dp[1];
-          dp[0]=dp[1];
-          dp[1]=dp[2];
-        }
-        return dp[2];
+    int fib(int n)
+    {
+        if(n==0 || n==1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
