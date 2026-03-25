9e303f31d355d840d1e5715940080122cb76f8dd
RS <rutujasakhare098@gmail.com>
2026-03-26 01:34:34 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (28.71%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
-    int solve(int i,int j,int m,int n){
-        if(i==m-1 && j==n-1) return 1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(i==m-1){
-            return solve(i , j+1 ,m ,n);
-        }
-        else if(j==n-1){
-            return solve(i+1 , j ,m ,n);
-        }
-       
-          int right=solve(i , j+1 ,m ,n);
-          int down=solve(i+1  , j,m ,n);
-        
-        return dp[i][j]=right+down;
+    //using tabulation
+    int uniquePaths(int m, int n) 
+    {
+        vector<vector<int>>grid(m,vector<int>(n,1));
+      
 
-    }
-    int uniquePaths(int m, int n)
-    {   dp.resize(m,vector<int>(n,-1));
-        return solve(0,0,m,n);
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+               grid[i][j]=grid[i-1][j]+grid[i][j-1];
+            }
+        }
+        return grid[m-1][n-1];
     }
 };
