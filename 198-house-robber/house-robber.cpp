e4105bb657a6697d80dae296dd0ab5c468fa85a7
RS <rutujasakhare098@gmail.com>
2026-04-03 12:42:34 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (21.38%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    //tabulation+memorization
+    int  n;
+    vector<int>dp;
+    int helper(vector<int>& nums ,int idx){
+        if(idx>=n) return INT_MIN;
+        if(idx==n-1) return nums[idx];
+        if(idx==n-2) return max(nums[idx],nums[idx+1]);
+        if(dp[idx]!=-1) return dp[idx];
+        int take=nums[idx]+helper(nums,idx+2);
+        int skip=helper(nums,idx+1);
+
+        return dp[idx]= max(take , skip);
+    }
     int rob(vector<int>& nums) 
-    {   
-        int n=nums.size();
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
-        vector<int>dp(3);
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[2]=max(dp[1] , (nums[i]+dp[0]));
-            dp[0]=dp[1];
-            dp[1]=dp[2];
-        }
-        return dp[2];
+    {
+       n=nums.size(); 
+       dp.resize(n,-1);
+       return helper(nums ,0);
     }
 };
