cda4b9c711f0fcdd022a9c9e223b65b9ab4ef7d8
RS <rutujasakhare098@gmail.com>
2026-04-07 20:28:26 +0530

    Time: 4 ms (56.11%) | Memory: 19 MB (29.79%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,34 +1,53 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, vector<int>>adj;
-         vector<int>inDegree(numCourses,0);
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
-            adj[v].push_back(u);
-            inDegree[u]++;
-        }
-
-        queue<int>q;
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0) q.push(i);
-        }
-       vector<int>ans;
-        while(!q.empty()){
-            int curr=q.front();
-            ans.push_back(curr);
-            q.pop();
-            for(int & neigh:adj[curr]){
-                inDegree[neigh]--;
-                if(inDegree[neigh]==0){
-                    q.push(neigh);
+  bool hasCycle;
+    void DFS(unordered_map<int ,vector<int>>& adj , int u ,  vector<bool>& visited,
+        vector<bool>& inRecursion , stack<int>& st){
+            visited[u]=true;
+            inRecursion[u]=true;
+            
+            for(int &neigh: adj[u]){
+                 if( inRecursion[neigh]) {
+                    hasCycle=true;
+                    return;
+                 }
+                 else if(!visited[neigh]){
+                    (DFS(adj , neigh ,visited,inRecursion,st ));
                 }
-
             }
-        }
-     if (ans.size()!=numCourses) return {};
-      return ans;
+             st.push(u);
+             inRecursion[u]=false;
+
+     
+
+    }
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         
+        unordered_map<int ,vector<int>>adj;
+        for(auto & s:prerequisites){
+            int u=s[0];
+            int v=s[1];
+
+            adj[v].push_back(u);
+
+        }
+      hasCycle=false;
+
+       stack<int>st;
+       vector<bool>visited(numCourses , false);
+       vector<bool>inRecursion(numCourses , false);
+
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                DFS(adj , i , visited,inRecursion , st);
+            }
+        }    
+        if(hasCycle==true) return {};
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+    return ans;
     }
 };
