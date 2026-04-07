9aa5471ff6205e4c555034693958ccc62d11673c
RS <rutujasakhare098@gmail.com>
2026-04-07 19:51:37 +0530

    Time: 5 ms (55.07%) | Memory: 19.9 MB (39.61%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
-    //Solved by BFS
-    //if topological order is there return true
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites)
-     {  unordered_map<int, vector<int>>adj;
-         vector<int>inDegree(numCourses,0);
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
-            adj[v].push_back(u);
-            inDegree[u]++;
-        }
+    bool isCycle(unordered_map<int ,vector<int>>& adj , int u ,  vector<bool>& visited,
+        vector<bool>& inRecursion){
+            visited[u]=true;
+            inRecursion[u]=true;
 
-        queue<int>q;
-        for(int i=0;i<numCourses;i++){
-            if(inDegree[i]==0) q.push(i);
-        }
-       vector<int>ans;
-        while(!q.empty()){
-            int curr=q.front();
-            ans.push_back(curr);
-            q.pop();
-            for(int & neigh:adj[curr]){
-                inDegree[neigh]--;
-                if(inDegree[neigh]==0){
-                    q.push(neigh);
+            for(int &neigh: adj[u]){
+                if(visited[neigh] && inRecursion[neigh]) return true;
+                else if(!visited[neigh]){
+                    if(isCycle(adj , neigh ,visited,inRecursion )) return true;
                 }
+            }
+             inRecursion[u]=false;
 
+        return false;
+
+    }
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        unordered_map<int ,vector<int>>adj;
+        for(auto & s:prerequisites){
+            int u=s[0];
+            int v=s[1];
+
+            adj[v].push_back(u);
+
+        }
+
+        vector<bool>visited(numCourses , false);
+        vector<bool>inRecursion(numCourses , false);
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(isCycle(adj , i , visited ,inRecursion )) return false;
             }
         }
-
-      if(ans.size()==numCourses) return true;
-      return false;
+        return true;
     }
 };
