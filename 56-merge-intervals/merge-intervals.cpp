fedc7779324250e1b11fcfc56d6ea81dc5514932
RutujaSakhare1220 <rutujasakhare098@gmail.com>
2026-07-10 23:03:09 +0530

    Time: 10 ms (13.83%) | Memory: 23.8 MB (59.52%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,40 +3,15 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         int n=intervals.size();
         sort(intervals.begin() , intervals.end());
-         vector<vector<int>> result;
-        vector<int>ans(2);
-
-        int v1=intervals[0][0];
-        int v2=intervals[0][1];
-        for(int i=1;i<n;i++)
-        {
-           int n1=intervals[i][0];
-           int n2=intervals[i][1];
-
-           if(n1<=v2){
-            v1=min(n1,v1);
-            v2=max(v2,n2);
+        vector<vector<int>>result;
+        result.push_back(intervals[0]);
+        for(int i=1;i<n;i++){
+           if(intervals[i][0]<=result.back()[1]){
+                  result.back()[1]=max(result.back()[1] , intervals[i][1]);
            }
-
-           else {
-
-            ans[0]=v1;
-            ans[1]=v2;
-            result.push_back(ans);
-            v1=n1;
-            v2=n2;
-
-           }
+           else  result.push_back(intervals[i]);
         }
 
-        // vector<int>m=result.back();
-        // if(m[0]==v1 && m[1]==v2) return result;
-        ans[0]=v1;
-        ans[1]=v2;
-        result.push_back(ans);
-
-
         return result;
-
     }
 };
