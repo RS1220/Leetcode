f7105b7771c287773d2166979a91bac3346a3969
RutujaSakhare1220 <rutujasakhare098@gmail.com>
2026-07-11 17:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (50.88%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,31 +1,71 @@
 class Solution {
 public:
-   //approach 2 using vectors
+   //approach 3 optimized
     void setZeroes(vector<vector<int>>& matrix) {
         int n=matrix.size();
         int m=matrix[0].size();
 
-        vector<bool>row(n,false);
-        vector<bool>col(m,false);
+        bool firstRow=false;
+        for(int j=0;j<m;j++){
+            if(matrix[0][j]==0){
+                firstRow=true;
+            }
+        }
 
+
+         bool firstCol=false;
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
+            if(matrix[i][0]==0){
+                firstCol=true;
+            }
+        }
+
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
                 if(matrix[i][j]==0){
-                    row[i]=true;
-                    col[j]=true;
-                    
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+
                 }
             }
         }
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(row[i] || col[j]){
-                   matrix[i][j]=0;
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][0]==0 ||
+                    matrix[0][j]==0){
+                        matrix[i][j]=0;
                     
+
                 }
             }
         }
+
+
+        
+          if( firstRow){
+
+            for(int j=0;j<m;j++){
+         matrix[0][j]=0;
+        }
+
+          }
+
+
+           if(firstCol){
+             for(int i=0;i<n;i++){
+           matrix[i][0]=0;
+               
+            
+        }
+
+           }
+       
+
+         
+
+
 
           return;
         
