981f5b0a5335e32daa45b635aecd98be579020ff
RutujaSakhare1220 <rutujasakhare098@gmail.com>
2026-07-12 20:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (31.09%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -2,18 +2,35 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
 
-        sort(s1.begin() , s1.end());
         int n=s1.size();
         int m=s2.size();
         if(n>m) return false;
-        for(int i=0;i<=m-n;i++){
-           string subString=s2.substr(i,n);
-           sort(subString.begin() , subString.end());
+        vector<int>v1(26,0);
+        vector<int>v2(26,0);
+        for(int i=0;i<n;i++){
+            v1[s1[i]-'a']++;
+        }
+        int i=0;
+        int j=0;
+        while(j<m){
+            v2[s2[j]-'a']++;
 
-           if(s1==subString) return true;
+            if(j-i+1>n){
+
+                 v2[s2[i]-'a']--;
+                 i++;
+
+            }
+
+            if(v1==v2) return true;
+
+
+            j++;
         }
 
-        return false;
+
+       return false;
+
         
     }
 };
