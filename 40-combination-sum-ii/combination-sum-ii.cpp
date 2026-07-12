982e28e9231bc88a23a2bb0dd229fb2ce637c78f
RutujaSakhare1220 <rutujasakhare098@gmail.com>
2026-07-13 01:06:36 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (28.87%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
-     vector<vector<int>> result;
+    vector<vector<int>> ans;
+    int n;
+  
+    void helper(vector<int>& candidates, vector<int>&v,int target,int idx){
 
-     void f(vector<int>& candidates, int target , int idx , vector<int>subset){
         if(target==0){
-            result.push_back(subset);
+            ans.push_back(v);
             return ;
         }
-        if(idx==candidates.size()) return;
+        if(target<0) return ;
 
-        if(candidates[idx]<=target){
+        for(int i=idx ;i<n;i++){
 
-            subset.push_back(candidates[idx]);
-            f(candidates , target-candidates[idx] , idx+1,subset);
-            subset.pop_back();
+            if( i>idx && candidates[i]==candidates[i-1]) continue;
+            int a=candidates[i];
+            v.push_back(a);
+            helper(candidates , v , target-a , i+1);
+             v.pop_back();
 
         }
-        int j=idx+1;
-        while(j<candidates.size() && candidates[j]==candidates[j-1]) j++;//if we ship one repeating number then we have to skip all repeating
-        f(candidates , target , j ,subset);//incase of skip ans candidates[idx]>target
-     }
+
+    }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-      sort(candidates.begin() , candidates.end());
-      vector<int>subset;
-      f(candidates , target ,0,subset);
-      return result;
-        
+         sort(candidates.begin() , candidates.end());
+        n=candidates.size();
+        vector<int>v;
+        helper(candidates , v , target , 0);
+
+
+        return ans;
     }
 };
