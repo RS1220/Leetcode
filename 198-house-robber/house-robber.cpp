0676040f12e036568975ad401ce16d1758f4229b
RutujaSakhare1220 <rutujasakhare098@gmail.com>
2026-09-22 13:23:40 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (23.35%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    int  n;
-    vector<int>dp;
-    int helper(vector<int>& nums ,int idx){
-        if(idx>=n) return INT_MIN;
-        if(idx==n-1) return nums[idx];
-        if(idx==n-2) return max(nums[idx],nums[idx+1]);
-        if(dp[idx]!=-1) return dp[idx];
-        int take=nums[idx]+helper(nums,idx+2);
-        int skip=helper(nums,idx+1);
+     int n;
+     vector<int>dp;
+    int helper(vector<int>& nums , int idx){
+        if(idx>=n) return 0;
+        // if(idx==n-1) return nums[n-1];
+        // if(idx==n-2) return max(nums[n-2] , nums[n-1]);
+        if(dp[idx]!=-1) return dp[idx] ;
 
-        return dp[idx]= max(take , skip);
+        int rob=nums[idx]+helper(nums , idx+2);
+        int skip=helper(nums , idx+1);
+
+        return dp[idx]=max(rob , skip);
+        
+
     }
-    int rob(vector<int>& nums) 
-    {
-       n=nums.size(); 
-       dp.resize(n,-1);
-       return helper(nums ,0);
+    int rob(vector<int>& nums) {
+ 
+         n=nums.size();
+         dp.resize(n,-1);
+        return helper(nums , 0);
+        
     }
 };
